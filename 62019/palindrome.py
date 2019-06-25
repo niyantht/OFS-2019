c4419aa1d4d3c25a21a9d3201f8192238246d2a6
Your Name <niyanththatta@gmail.com>
2019-06-25 11:46:45 -0400

    First commitment

---
+++ 62019/palindrome.py
@@ -0,0 +1,8 @@
+string = input("enter string:")
+rev_string = string[::-1]
+if string == rev_string: 
+    print ("string is a palindrome")
+else:
+    print ("string is not a palindrome")
+    
+    
---
+++ 62019/primenumbers.py
@@ -0,0 +1,12 @@
+#this program will check whether the given number is prime
+num = int(input("enter a positive number")
+if num>1
+    for i in range (2,num):
+        if (num%i)==0:
+            print("not prime")
+            break
+    else:
+        print("prime")
+else:
+    print("not prime")            
+          
